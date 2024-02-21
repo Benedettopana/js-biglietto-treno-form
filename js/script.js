50a4875848d3fb85683cba8e22e9cdb37a38c029
Benedettopana <benedetto.pana@gmail.com>
2024-02-21 17:04:05 +0100

    [add]milestone 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let myAge;
 let myKm;
+let myName;
 // Prezzo biglietto normale
 let ticketPrice;
 // Prezzo biglietto scontato
@@ -7,17 +8,23 @@ let ticketSale;
 // tassa standard Euro per KM 
 const rate = 0.21;
 // Sconto base è 0
-let sale = true;
+let sale;
+let ticketNumb;
+let car;
 
+// const prova2 = document.querySelector('body prova2');
+// let prova2 = element.classList;
 // INPUT
 const inputAge = document.querySelector('.inputAge');
 const inputKm = document.querySelector('.inputKm');
+const inputName = document.querySelector('.inputName');
 
 // Bottoni
 const btnSend = document.querySelector('.btnSend');
 
 //Prendo i valori dall'utente.
 btnSend.addEventListener('click',function(event){
+  // prova2.classList.add('d-block');
   // neurtralizzo la action del form
   event.preventDefault();
   // leggo il contenuto dell'input
@@ -26,6 +33,7 @@ btnSend.addEventListener('click',function(event){
   // myAge.innerHTML = inputAge.value;
   myAge = parseInt(inputAge.value);
   myKm = parseInt(inputKm.value);
+  myName = inputName.value;
 
   // Calcolo prezzo del biglietto standard
   ticketPrice = myKm * rate;
@@ -33,22 +41,57 @@ btnSend.addEventListener('click',function(event){
   // Controllo se posso applicare sconti
   if(myAge < 18){
     ticketSale = (ticketPrice*20)/100;
-    ticketPrice = ticketPrice.toFixed(2);
+    ticketPrice = ticketSale.toFixed(2);
+    sale = 'Offerta under 18 applicata';
   }else if(myAge >= 65){
     ticketSale = (ticketPrice*40)/100;
-    ticketPrice = ticketPrice.toFixed(2);
+    ticketPrice = ticketSale.toFixed(2);
+    sale = 'Offerta over 65 applicata';
   }else{
-    sale = false
+    ticketPrice = ticketPrice.toFixed(2);
+    sale = 'Nessuna offerta applicata.';
   }
 
-  
+  // trovo il tipo di offerta
+
   
 
+
   // Stampo i risultati in console
+  console.log(myName);
   console.log(myAge);
   console.log(myKm);
   console.log(ticketPrice);
+
+  // faccio apparire il biglietto
+  
+
+  // CREO a video il biglietto
+  document.getElementById('nomeGenerato').innerHTML = myName;
+
+  //Offerta
+  document.getElementById('offer').innerHTML = sale;
+  // n carrozza random
+  document.getElementById('train').innerHTML = 
+  car = Math.floor((Math.random() * 15) + 1);
+
+  // COD ticket random
+  document.getElementById('codTicket').innerHTML = 
+  ticketNumb = Math.floor(Math.random() * (999999 - 100000 + 1) ) + 100000;
+
+  // costo biglietto
+  document.getElementById('cost').innerHTML = 
+  `
+  <p>
+    ${ticketPrice}&#8364;
+  </p>
+  `;
+  
+  
 })
+
+// tasto reset
+
 
 
 
